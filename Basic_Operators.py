bd575a5853c8de4fb63af171a9081d81c0233298
alone <154563439+neverfriendme@users.noreply.github.com>
2024-02-24 20:14:39 -0500

    Create Basic_Operators.py
    basic operators

---
+++ Basic_Operators.py
@@ -0,0 +1,19 @@
+#Operators:1 == 1 your telling the computer that 1=1
+#5+2 your telling the computer to add
+#6-1 your telling the computer to substract
+# =:one equal sign is used normally for variables:hello = 5 print(hello)
+#6/3 your telling the computer to divde 6 by 3
+hello = 5
+print(hello)
+
+if 1==1:
+  while True:
+    print("False")
+
+print(5+2)
+
+print(6-1)
+
+print(6/3)
+
++
